735b1bb0032aadc00ed0bb9d70bc95a796a93cde
Ian King <jamesianking@hotmail.com>
2026-02-03 22:19:28 -0800

    Remove navigationAgent, begin implementing breadcrumbs for nav. Not yet working around walls

--- Enemy.cs
+++ Enemy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 using Godot;
 
 public partial class Enemy : CharacterBody2D
@@ -12,28 +13,12 @@ public partial class Enemy : CharacterBody2D
     // Are we aggroed to the MainChar?
     private bool _isAggro = false;
 
-    [Export]
-    NavigationAgent2D _navigationAgent;
-
-    private void OnVelocityComputed(Vector2 safeVelocity)
-    {
-        Velocity = safeVelocity;
-        MoveAndSlide();
-    }
-
-    public override void _Ready()
-    {
-        _navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
-        _navigationAgent.VelocityComputed += OnVelocityComputed;
-    }
+    public override void _Ready() { }
 
     public override void _Process(double delta)
     {
         // Find the MainChar - assumes Party is at same depth as Enemy
         _mainChar ??= GetNode<MainCharControl>("../Party/MainChar");
-
-        // Track the player's current position
-        _navigationAgent.TargetPosition = _mainChar.GlobalTransform.Origin;
 
         // Check distance to MainChar
         Vector2 charLoc = _mainChar.GlobalTransform.Origin;
@@ -61,48 +46,45 @@ public partial class Enemy : CharacterBody2D
 
     public void MoveTowardPlayer()
     {
-        // var spaceState = GetWorld2D().DirectSpaceState;
-        // var query = PhysicsRayQueryParameters2D.Create(
-        //     GlobalTransform.Origin,
-        //     _mainChar.GlobalTransform.Origin
-        // );
+        var spaceState = GetWorld2D().DirectSpaceState;
+        var query = PhysicsRayQueryParameters2D.Create(
+            GlobalTransform.Origin,
+            _mainChar.GlobalTransform.Origin
+        );
 
-        // Do not query when the map has never synchronized and is empty.
-        if (NavigationServer2D.MapGetIterationId(_navigationAgent.GetNavigationMap()) == 0)
+        // Exclude self from the ray-cast result
+        query.Exclude = [GetRid()];
+        Godot.Collections.Dictionary result = spaceState.IntersectRay(query);
+
+        // If the player is visible, move toward it
+        if (result.Count > 0)
         {
-            return;
-        }
+            if (((ulong)result["collider_id"]) == _mainChar.GetInstanceId())
+            {
+                Velocity = Speed * Position.DirectionTo(_mainChar.GlobalTransform.Origin);
+                MoveAndSlide();
+            }
+            else // Move around walls by tracking breadcrumbs
+            {
+                foreach (Vector2 crumb in _mainChar.breadcrumbs)
+                {
+                    // Raycast to see if accessible
+                    PhysicsRayQueryParameters2D ray = PhysicsRayQueryParameters2D.Create(
+                        GlobalTransform.Origin,
+                        crumb
+                    );
+                    ray.Exclude = [GetRid()];
+                    var crumbResult = spaceState.IntersectRay(ray);
 
-        if (_navigationAgent.IsNavigationFinished())
-        {
-            return;
+                    // Nothing is obstructing this movement - go toward this
+                    if (crumbResult.Count == 0)
+                    {
+                        Velocity = Speed * Position.DirectionTo(_mainChar.GlobalTransform.Origin);
+                        MoveAndSlide();
+                        break;
+                    }
+                }
+            }
         }
-
-        Vector2 nextPathPosition = _navigationAgent.GetNextPathPosition();
-        Vector2 newVelocity = GlobalPosition.DirectionTo(nextPathPosition) * Speed;
-        if (_navigationAgent.AvoidanceEnabled)
-        {
-            _navigationAgent.Velocity = newVelocity;
-        }
-        else
-        {
-            OnVelocityComputed(newVelocity);
-        }
-
-        // // Exclude self from the ray-cast result
-        // query.Exclude = [GetRid()];
-        // Godot.Collections.Dictionary result = spaceState.IntersectRay(query);
-
-        // // If the player is visible, move toward it
-        // if (result.Count > 0)
-        // {
-        //     if (((ulong)result["collider_id"]) == _mainChar.GetInstanceId())
-        //     {
-        //         Velocity = Speed * Position.DirectionTo(_mainChar.GlobalTransform.Origin);
-        //         MoveAndSlide();
-        //     }
-        //     else // Move around walls if possible
-        //     { }
-        // }
     }
 }
--- MainCharControl.cs
+++ MainCharControl.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 public partial class MainCharControl : CharacterBody2D
@@ -9,13 +11,23 @@ public partial class MainCharControl : CharacterBody2D
     // Object near enough to interact with - set by other object
     public TalkNpc intObject;
 
-    // Text box to scroll through
+    // Text box to scroll through - belongs to NPC
     public SpeechText intTextBox;
+
+    // Breadcrumbs - recent locations for enemies to follow
+    public Queue<Vector2> breadcrumbs;
 
     public override void _PhysicsProcess(double delta)
     {
         GetInput();
         MoveAndSlide();
+        ProcessBreadcrumb();
+    }
+
+    public override void _Ready()
+    {
+        // Initialize breadcrumbs queue
+        breadcrumbs = new Queue<Vector2>();
     }
 
     public void GetInput()
@@ -34,6 +46,33 @@ public partial class MainCharControl : CharacterBody2D
         {
             SpeechText intTextScript = intTextBox as SpeechText;
             intTextScript.ScrollDown();
+        }
+    }
+
+    public void ProcessBreadcrumb()
+    {
+        Vector2 curPos = GlobalTransform.Origin;
+
+        if (breadcrumbs.Count > 0)
+        {
+            Vector2 prevCrumb = breadcrumbs.Last();
+
+            // Calculate vector distance between current player location and previous breadcrumb location
+            float vectorDist =
+                Math.Abs(Math.Abs(curPos.X) - Math.Abs(prevCrumb.X))
+                + Math.Abs(Math.Abs(curPos.Y) - Math.Abs(prevCrumb.Y));
+
+            // If far enough, add a new breadcrumb
+            if (vectorDist > 2)
+            {
+                breadcrumbs.Enqueue(curPos);
+
+                // If queue is long enough, remove the earliest entry
+                if (breadcrumbs.Count > 10)
+                {
+                    breadcrumbs.Dequeue();
+                }
+            }
         }
     }
 }
